dc00051d55ccd5a3de59115ce6068544c72e539e
Mohamed <mohamedabuassaf@gmail.com>
2024-05-24 21:11:38 +0300

    added the db connection config

---
+++ backend/db.py
@@ -0,0 +1,29 @@
+import psycopg2
+from ..config import db_config
+
+
+def db_connection():
+    conn = psycopg2.connect(**db_config)
+    return conn
+
+def init_table():
+    conn = db_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            username VARCHAR(100) NOT NULL,
+            password VARCHAR(150) NOT NULL
+        )
+    ''')
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+if __name__ == '__main__':
+    init_table()
+    
